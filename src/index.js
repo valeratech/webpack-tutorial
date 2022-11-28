05cf1596856b4035d31d4cd1440934708e44534f
Ryan Valera <valeraryan@gmail.com>
2022-11-27 18:02:51 -0800

    Added a simple return name function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
+import myName from './myName'
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is rquired for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.textContent = myName('Cody');
 
     return element;
 }
